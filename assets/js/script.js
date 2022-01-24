1ac7d8a5c9144d06ee8b96fdcf67446df680c756
rachelsaito <rachel.saito.vdl@gmail.com>
2022-01-23 21:19:35 -0800

    Added to CSS page

--- assets/js/script.js
+++ assets/js/script.js
@@ -146,7 +146,7 @@ function saveHighscore() {
     window.localStorage.setItem("highscores", JSON.stringify(highscores));
 
     // redirect to next page
-    window.location.href = "highscores.html";
+    window.location.href = "index.html";
   }
 }
 
