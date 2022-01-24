b26c305e9510b1f2a55be15cf5c65982995f7309
rachelsaito <rachel.saito.vdl@gmail.com>
2022-01-23 22:34:58 -0800

    updated questions.js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,35 +1,23 @@
 //list of questions
 var questions = [
     {
-     Qtitle: "What language is this page using for logic?",
-        optionA: "A",
-        optionB: "B",
-        optionC: "C",
-        optionD: "D",
-        correctAnswer: "A"
+        title: "What can arrays in JavaScript store?",
+        choices: ["other arrays", "booleans", "numbers and strings", "all of the above"],
+        answer: "all of the above"
     },
     {
-    Qtitle: "What language is used for styling web pages?",
-        optionA: "Apple",
-        optionB: "CSS",
-        optionC: "Bootstrap",
-        optionD: "HTML",
-        correctAnswer: "B"
+        title: "What language is used for styling web pages?",
+        choices: ["Apple", "CSS", "Bootstrap", "HTML"],
+        answer: "CSS"
     },
     {
-    Qtitle: "What markup language acts as the framework for a web page?",
-        optionA: "CSS",
-        optionB: "JavaScript",
-        optionC: "HTML",
-        optionD: "API",
-        correctAnswer: "C"
+        title: "What markup language acts as the framework for a web page?",
+        choices: ["CSS", "JavaScript", "HTML", "API"],
+        answer: "HTML"
     },
     {
-    Qtitle: "Which of these is a scripting language?",
-        optionA: "html",
-        optionB: "css",
-        optionC: "english",
-        optionD: "javascript",
-        correctAnswer: "D"
+        title: "Which of these is a scripting language?",
+        choices: ["html", "css", "english", "javascript"],
+        answer: "javascript"
     }
-]
+];
