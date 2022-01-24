49322152025dc2cb39cad263553e63193bc8fff6
rachelsaito <rachel.saito.vdl@gmail.com>
2022-01-23 22:04:10 -0800

    reorganzied script and html

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,7 +61,7 @@ function questionClick() {
   // check if user guessed wrong
   if (this.value !== questions[currentQuestionIndex].answer) {
     // penalize time
-    time -= 15;
+    time -= 10;
 
     if (time < 0) {
       time = 0;
@@ -70,13 +70,8 @@ function questionClick() {
     // display new time on page
     timerEl.textContent = time;
 
-    // play "wrong" sound effect
-    sfxWrong.play();
-
     feedbackEl.textContent = "Wrong!";
   } else {
-    // play "right" sound effect
-    sfxRight.play();
 
     feedbackEl.textContent = "Correct!";
   }
@@ -164,33 +159,3 @@ submitBtn.onclick = saveHighscore;
 startBtn.onclick = startQuiz;
 
 initialsEl.onkeyup = checkForEnter;
-
-function printHighscores() {
-  // either get scores from localstorage or set to empty array
-  var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
-
-  // sort highscores by score property in descending order
-  highscores.sort(function(a, b) {
-    return b.score - a.score;
-  });
-
-  highscores.forEach(function(score) {
-    // create li tag for each high score
-    var liTag = document.createElement("li");
-    liTag.textContent = score.initials + " - " + score.score;
-
-    // display on page
-    var olEl = document.getElementById("highscores");
-    olEl.appendChild(liTag);
-  });
-}
-
-function clearHighscores() {
-  window.localStorage.removeItem("highscores");
-  window.location.reload();
-}
-
-document.getElementById("clear").onclick = clearHighscores;
-
-// run function when page loads
-printHighscores();
